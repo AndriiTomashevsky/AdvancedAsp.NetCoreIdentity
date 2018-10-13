6adb51214efed57e7c15c02565571187c38c5e9f
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-10-13 20:35:22 +0300

    Adding Custom User Properties

--- Users/Controllers/HomeController.cs
+++ Users/Controllers/HomeController.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Users.Models;
 
 namespace Users.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private UserManager<AppUser> userManager;
+
+        public HomeController(UserManager<AppUser> userMgr)
+        {
+            userManager = userMgr;
+        }
+
         [Authorize]
         public IActionResult Index()
         {
@@ -16,7 +27,7 @@ namespace Users.Controllers
         [Authorize(Roles = "Users")]
         public IActionResult OtherAction()
         {
-            return View("Index",GetData(nameof(OtherAction)));
+            return View("Index", GetData(nameof(OtherAction)));
         }
 
         private Dictionary<string, object> GetData(string actionName)
@@ -27,8 +38,35 @@ namespace Users.Controllers
                 ["User"] = HttpContext.User.Identity.Name,
                 ["Authenticated"] = HttpContext.User.Identity.IsAuthenticated,
                 ["Auth Type"] = HttpContext.User.Identity.AuthenticationType,
-                ["In Users Role"] = HttpContext.User.IsInRole("Users")
+                ["In Users Role"] = HttpContext.User.IsInRole("Users"),
+                ["City"] = CurrentUser.Result.City,
+                ["Qualification"] = CurrentUser.Result.Qualifications
             };
         }
+
+        [Authorize]
+        public async Task<IActionResult> UserProps()
+        {
+            return View(await CurrentUser);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> UserProps([Required]Cities city, 
+            [Required]QualificationLevels qualifications)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await CurrentUser;
+                user.City = city;
+                user.Qualifications = qualifications;
+                await userManager.UpdateAsync(user);
+                return RedirectToAction("Index");
+            }
+
+            return View(await CurrentUser);
+        }
+
+        private Task<AppUser> CurrentUser => userManager.FindByNameAsync(HttpContext.User.Identity.Name);
     }
 }
